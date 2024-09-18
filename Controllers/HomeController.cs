45888dc925c7d862d6d3280a446aa904fabd39ab
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-09-18 20:33:46 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AjaxBasicCRUDDemo.Models;
+
+namespace AjaxBasicCRUDDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Item> items = new List<Item>()
+        {
+            new Item {Id = 1, Name="Item 1", Description="Description 1", Price=10.00m},
+            new Item {Id = 2, Name="Item 2", Description="Description 2", Price=20.00m},
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAllItems()
+        {
+            if (items.Count > 0)
+            {
+                return Json(items, JsonRequestBehavior.AllowGet);
+            }
+            return new HttpStatusCodeResult(500);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Item item)
+        {
+            item.Id = items.Max(i => i.Id) + 1;
+            items.Add(item);
+            return Json(item);
+        }
+
+        public ActionResult GetItem(int id)
+        {
+            var item = items.FirstOrDefault(i => i.Id == id);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            var existingItem = items.FirstOrDefault(i => i.Id == item.Id);
+            if (existingItem != null)
+            {
+                existingItem.Name = item.Name;
+                existingItem.Description = item.Description;
+                existingItem.Price = item.Price;
+            }
+            return Json(existingItem);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = items.FirstOrDefault(i => i.Id == id);
+            items.Remove(item);
+            return Json(item);
+        }
+    }
+}
---
+++ Scripts/myCrud.js
@@ -0,0 +1,67 @@
+﻿function loadItems() {
+    $.ajax({
+        url: "/Home/GetAllItems",
+        type: "GET",
+        success: function (data) {
+            $("#tblBody").empty()
+
+            $.each(data, function (index, item) {
+                var row = `<tr>
+                <td>${item.Name}</td>
+                 <td>${item.Description}</td>
+                  <td>${item.Price}</td>
+                   <td>
+                   <button onclick="editItem(${item.Id})" value="Edit" class="btn btn-success">Edit</button>
+                   </td>
+                   <td>
+                   <button onclick="deleteRecord(${item.Id})" value="Delete" class="btn btn-danger">Delete</button>
+                   </td>
+                   </tr>`
+                   $("#tblBody").append(row)
+            })
+        },
+        error: function (err) {
+            $("#tblBody").empty()
+            alert("No Data Available")
+        }
+    })
+}
+
+//toggling views
+$("#btnAdd").click(() => {
+    $("#itemList").hide();
+    $("#newRecord").show();
+})
+
+function addNewRecord(newItem) {
+    $.ajax({
+        url: "/Home/Add",
+        type: "POST",
+        data: newItem,
+
+        success: function (item) {
+            alert("New Item Added Successfully")
+            loadItems()
+        },
+        error: function (err) {
+            alert("Error Adding New Record")
+        }
+    })
+}
+
+function deleteRecord(itemId) {
+    if (confirm("Do you wish to delete this item?")) {
+        $.ajax({
+            url: "/Home/Delete",
+            type: "POST",
+            data: { id: itemId },
+            success: function (item) {
+                alert("Record Deleted Sucessfully")
+                loadItems();
+            },
+            error: function (err) {
+                alert("Record Does Not Exist")
+            }
+        })
+    }
+}
